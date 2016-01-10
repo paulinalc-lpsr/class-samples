c884586be864c9a036e74cfe441cb16a97c9525f
LPS Richmond Student <lps@lps-1011PX.(none)>
2016-01-10 15:54:08 -0800

    square pattern

---
+++ drawColorGrid.py
@@ -0,0 +1,68 @@
+import turtle
+
+def drawRedSquare(myTurtle):
+	myTurtle.color("red")
+	count = 0 
+	while count < 4:
+		myTurtle.forward(20)
+		myTurtle.left(90)
+		count = count + 1
+
+def drawBlueSquare(myTurtle):
+	myTurtle.color("blue")
+	myTurtle.penup()
+	myTurtle.forward(20)
+	myTurtle.pendown()
+	count = 0
+	while count < 4:
+		myTurtle.forward(20)
+		myTurtle.left(90)
+		count = count + 1
+
+def drawGreenSquare(myTurtle):
+	myTurtle.color("green")
+	myTurtle.penup()
+	myTurtle.forward(20)
+	myTurtle.left(90)
+	myTurtle.forward(20)
+	myTurtle.pendown()
+	count = 0
+	while count < 4:
+		myTurtle.forward(20)
+		myTurtle.right(90)
+		count = count + 1
+
+def drawYellowSquare(myTurtle):
+	myTurtle.color("yellow")
+	myTurtle.penup()
+	myTurtle.forward(20)
+	myTurtle.left(90)
+	myTurtle.pendown()
+	count = 0
+	while count < 4:
+		myTurtle.forward(20)
+		myTurtle.left(90)
+		count = count + 1
+
+def drawAllSquares(myTurtle):
+	drawRedSquare(myTurtle)
+	drawBlueSquare(myTurtle)
+	drawGreenSquare(myTurtle)
+	drawYellowSquare(myTurtle)
+
+def drawSquarePattern(myTurtle):
+	count = 0
+	while count < 5:
+		drawAllSquares(shawn)
+		myTurtle.left(90)
+		myTurtle.penup()
+		myTurtle.forward(5)
+		myTurtle.left(90)
+		myTurtle.pendown()
+		count = count + 1
+
+shawn = turtle.Turtle()
+
+drawSquarePattern(shawn)
+
+turtle.exitonclick()
